781b4f5206fe571cc2a4505dfdfe0ab7f3fd76b8
Jim Speakman <jim.speakman@cybg.com>
2017-05-31 17:30:36 +0100

    Reactive web clien test

--- src/test/kotlin/fixedgigha/boot2/Boot2ApplicationTests.kt
+++ src/test/kotlin/fixedgigha/boot2/Boot2ApplicationTests.kt
@@ -3,13 +3,21 @@ package fixedgigha.boot2
 import fixedgigha.boot2.model.Person
 import kotlin.jvm.javaClass
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.http.HttpStatus
 import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.http.MediaType.APPLICATION_JSON
+import java.time.Duration
+import org.springframework.web.reactive.function.client.bodyToFlux
+import reactor.core.publisher.Flux
+import reactor.core.publisher.test
 
 @RunWith(SpringRunner::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -33,4 +41,29 @@ class Boot2ApplicationTests {
         Assert.assertEquals(HttpStatus.OK, result.statusCode)
         println("People ${result.body}")
     }
+
+    @LocalServerPort
+    var port : Int? = null
+
+    lateinit var client: WebClient
+
+    @Before
+    fun setup() {
+        client = WebClient.create("http://localhost:$port")
+    }
+
+    @Test
+    fun reactLoadPeople() {
+        val flux : Flux<Person> = client.get().uri("/people?lastName=Speakman").accept(APPLICATION_JSON)
+                .retrieve().bodyToFlux()
+
+        flux.test().consumeNextWith {
+            println("Consumed ${it.firstName}")
+            Assert.assertEquals("Jim", it.firstName)
+        }.verifyComplete()
+
+
+    }
+
+
 }
